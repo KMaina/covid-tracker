4e26e21378b3ff80f4c2e6059e3447796254cb98
gkarumbi <gkarumbi@gmail.com>
2020-08-25 15:23:47 +0300

    Added login page to allow our users to login into the system

--- covid19/urls.py
+++ covid19/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.auth import views
 
 urlpatterns = [
     url(r'', include('corona.urls')),
     url('admin/', admin.site.urls),
     url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^logout/$', views.logout, {"next_page": '/'}), 
 ]
