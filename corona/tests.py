a51065b271b4a756ef7687de572cfcb16af8b1ed
Muia23 <wilfred.muema@gmail.com>
2020-08-26 15:22:05 +0300

    clone repo

--- corona/tests.py
+++ corona/tests.py
@@ -1,3 +1,51 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class BaseTest(TestCase):
+    def setUp(self):
+        self.register_url=reverse('signup')
+        self.user = {
+            'email' : 'tester@mail.com',
+            'username' : 'tester',
+            'password' : 'testerpass',
+            'password2' : 'testerpass',            
+            'fullname' : 'testerfull',
+        }
+
+        self.user_short_password = {
+            'email' : 'tester@mail.com',
+            'username' : 'tester',
+            'password' : 'te',
+            'password2' : 'te',            
+            'fullname' : 'testerfull',
+        }
+        self.user_unmatch_password = {
+            'email' : 'tester@mail.com',
+            'username' : 'tester',
+            'password' : 'te',
+            'password2' : 'te',            
+            'fullname' : 'testerfull',
+        }
+        return super().setUp()
+
+class RegisterTest(BaseTest):
+    #Test register page loads properly
+    def test_page_load(self):
+        response=self.client.get(self.register_url)
+        self.assertEqual(response.status_code,200)
+        self.assertTemplateUsed(response,'registration/registration_form.html')
+    
+    #Test user can register
+    def test_user_can_register(self):
+        response=self.client.post(self.register_url,self.user,format='text/html')
+        self.assertEqual(response.status_code,302)
+    
+    #Test user can't register with a short-password
+    def test_user_short_password(self):
+        response=self.client.post(self.register_url,self.user_short_password,format='text/html')
+        self.assertEqual(response.status_code,302)
+
+    #Test user can't register with a unmatching-passwords
+    def test_user_unmatch_passwords(self):
+        response=self.client.post(self.register_url,self.user_unmatch_password,format='text/html')
+        self.assertEqual(response.status_code,302)
--- covid19/urls.py
+++ covid19/urls.py
@@ -14,11 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
 from django.conf.urls import url, include
-from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'', include('corona.urls')),
-    path('admin/', admin.site.urls),
+    url('admin/', admin.site.urls),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
+    url(r'^password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        auth_views.password_reset_confirm, name='password_reset_confirm'),
+    url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
 ]
+
