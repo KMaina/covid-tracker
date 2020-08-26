97e2580440d7182ce086e6930e714e9b4e7b3cd8
gkarumbi <gkarumbi@gmail.com>
2020-08-26 14:12:50 +0300

    .

--- covid19/urls.py
+++ covid19/urls.py
@@ -14,11 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.auth import views 
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'', include('corona.urls')),
-    path('admin/', admin.site.urls),
+    url(r'admin/', admin.site.urls),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^logout/$', auth_views.LogoutView.as_view(), {'next_page': '/'}, name='logout'),
 ]
