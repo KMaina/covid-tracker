4cd440852998be8ad5897c774a09811569ef4a21
edumorris <eduuwachira@gmail.com>
2020-08-27 16:27:29 +0300

    Covid19 API, firstpage view - to be refined

--- corona/views.py
+++ corona/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+import requests
 
 # Create your views here.
 def home(request):
-    pass
+    cov_response = requests.get('https://api.thevirustracker.com/free-api?countryTotals=ALL')
+    cov_data = cov_response.json()
+    ref_data = cov_data['countryitems']
+    
+    return render(request, 'covid/home.html', {"cov_data": ref_data})
 
 def index_test(request):
     title = "Covid Index Bootstrap Test"
