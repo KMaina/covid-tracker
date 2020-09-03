9082dc2ffd8000ece528551bc3d0deb1011c9115
edumorris <eduuwachira@gmail.com>
2020-09-03 14:55:48 +0300

    Email backend fix

--- covid19/settings.py
+++ covid19/settings.py
@@ -167,6 +167,8 @@ EMAIL_PORT = config('EMAIL_PORT')
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
 AUTH_USER_MODEL = 'corona.User'
 
 from django.urls import reverse_lazy
